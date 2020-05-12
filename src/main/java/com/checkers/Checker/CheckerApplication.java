d60d9604d49111653f57089fad4a9d98446307f9
eliyaoo32 <eliyaoo32@gmail.com>
2020-05-12 17:20:59 +0300

    Init Springs

---
+++ src/main/java/com/checkers/Checker/CheckerApplication.java
@@ -0,0 +1,13 @@
+package com.checkers.Checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckerApplication.class, args);
+	}
+
+}
