5f07f6a6ce042ee105d62368033e50b1a066544b
Marina Rappoport <rappoportmarina@gmail.com>
2020-06-28 08:30:27 +0300

    added game_start and move support to WebSocket

---
+++ src/main/java/com/checkers/server/utils/GameStorage.java
@@ -0,0 +1,33 @@
+package com.checkers.server.utils;
+
+import com.checkers.server.logic.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Singleton class to store games that currently is playing
+ */
+public class GameStorage {
+	private static GameStorage ourInstance = new GameStorage();
+	private List<Game> games = new ArrayList<>();
+
+	public static GameStorage getInstance() {
+		return ourInstance;
+	}
+
+	private GameStorage() {
+	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public Game findGameByPlayerName(String userName) {
+		for (Game game : games) {
+			if (game.getBlack().getName().equals(userName) || game.getWhite().getName().equals(userName))
+				return game;
+		}
+		return null;
+	}
+}
