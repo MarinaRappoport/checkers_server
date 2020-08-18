1a918c582b1d6eb7ec9194cf9fdd0db1f43b3c95
eliyaoo32 <eliyaoo32@gmail.com>
2020-08-18 14:46:33 +0300

    Reserve static files

---
+++ src/main/resources/public/precache-manifest.35e7ed11f0a7d98a5b26879368fd662a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbb235c2b07ddc258c34b3cc1d1dde3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a42676ee45bd79cc3c7",
+    "url": "/static/css/main.da6d22e4.chunk.css"
+  },
+  {
+    "revision": "8e5d6aaae6ecc1587f1f",
+    "url": "/static/js/2.84944cbe.chunk.js"
+  },
+  {
+    "revision": "85e5a90b9b2e69e181593a98295ecff0",
+    "url": "/static/js/2.84944cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a42676ee45bd79cc3c7",
+    "url": "/static/js/main.456b7532.chunk.js"
+  },
+  {
+    "revision": "920bf69ce6394e4fdab8",
+    "url": "/static/js/runtime-main.59255b02.js"
+  }
+]);
